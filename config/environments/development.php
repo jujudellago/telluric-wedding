bf8e576aa7e828c3a09075144fdfd328fe55f0a0
Julien Ramel <julien.ramel@gmail.com>
2020-03-31 18:42:16 +0700

    added plugins

--- config/environments/development.php
+++ config/environments/development.php
@@ -3,12 +3,16 @@
  * Configuration overrides for WP_ENV === 'development'
  */
 
+
+
+
 use Roots\WPConfig\Config;
 
 Config::define('SAVEQUERIES', true);
 Config::define('WP_DEBUG', true);
 Config::define('WP_DEBUG_DISPLAY', true);
 Config::define('SCRIPT_DEBUG', true);
+Config::define('WPLANG','fr');
 
 ini_set('display_errors', 1);
 
